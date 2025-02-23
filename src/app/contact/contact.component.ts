f61ca5405b48fb98df5093f7239c1669ace36971
mikelopresti21 <mikelopresti21@gmail.com>
2025-02-23 09:37:36 -0500

    added contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
   standalone: true
