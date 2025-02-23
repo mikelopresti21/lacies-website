58774405d57381336e0d1b0d789afcd90d413f56
mikelopresti21 <mikelopresti21@gmail.com>
2025-02-23 17:48:32 -0500

    added alert windows for email submission

--- src/app/services/emails.service.ts
+++ src/app/services/emails.service.ts
@@ -14,8 +14,13 @@ export class EmailsService {
     console.log('from email service');
     return this.http.post(this.apiUrl, formData)
       .subscribe({
-        next: response => console.log('success', response),
-        error: error => console.error(error)
+        next: (response) => {
+          window.alert('Thanks for reaching out to me! I will follow up with you as soon as I can!');
+        },
+        error: (error) => {
+          window.alert("Oh no something went wrong! Please make sure you are entering a valid email address");
+          console.error(error);
+        }
       });
   }
 }
