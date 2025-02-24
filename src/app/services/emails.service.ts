736c031b0245de5f0c0a67b58739682d699b2078
mikelopresti21 <mikelopresti21@gmail.com>
2025-02-23 20:55:11 -0500

    test

--- src/app/services/emails.service.ts
+++ src/app/services/emails.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EmailsService {
 
-  private apiUrl = "https://lacies-website.vercel.app/contact"
+  private apiUrl = "https://www.lacieperez.com/contact"
   // private apiUrl = "http://localhost:3000/contact"
 
   constructor(private http: HttpClient) { }
